860186239720db6707d9f56bc007cbdac3c5c345
liz <lizanderson.ca@gmail.com>
2022-05-29 15:30:24 -0700

    start looping through json file. add more endpoints

--- app.js
+++ app.js
@@ -15,14 +15,42 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-var data = require('./data/data.json'); //(with path)
+var data = require('./data/data.json'); 
+
 // per state? parameter as state?
 app.get('/getFirstPrimaryCandidatePerCounty', (req, res) => {
+  var jsonArr = '';
   // return the winning primary candidates in each county
   // loop state, county
   // combine all candidates from each party, sort by candidates, get first
   // return county, 1st candidate, number of votes
-    res.json(data);
+
+  for (var state of Object.keys(data)) {
+    var stateData = data[state];
+    for (var county of Object.keys(stateData)) {
+      var countyData = stateData[county]
+      for (var party of Object.keys(countyData)) {
+        var partyData = countyData[party];
+        jsonArr = party;
+      }
+    }
+  }
+  res.send(jsonArr);
+});
+
+
+app.get('/getFirstPrimaryCandidatePerState', (req, res) => {
+  // return the winning primary candidates in each state
+  // loop state, county
+  // combine all candidates from each party, sort by candidates, get first
+  // return state, 1st candidate, party, number of votes
+  res.json(data);
+});
+
+app.get('/getFirstPrimaryCandidateOverall', (req, res) => {
+  // return the winning primary candidates overall
+  // return 1st candidate 
+  res.json(data);
 });
 
 app.get('/', (req, res) => {
@@ -30,10 +58,3 @@ app.get('/', (req, res) => {
 })
 
 
-// return the winning primary candidates in each state
-// loop state, county
-// combine all candidates from each party, sort by candidates, get first
-// return state, 1st candidate, party, number of votes
-
-// return the winning primary candidates overall
-// return 1st candidate 
